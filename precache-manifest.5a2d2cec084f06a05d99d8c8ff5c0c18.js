726f9235c2cbfa01950e050819ff454a6f4a4a05
Ahmet Ramazan Çapoğlu <52932954+cap-oglu@users.noreply.github.com>
2023-08-22 22:24:53 +0300

    Updates

--- precache-manifest.5a2d2cec084f06a05d99d8c8ff5c0c18.js
+++ precache-manifest.5a2d2cec084f06a05d99d8c8ff5c0c18.js
@@ -32,20 +32,20 @@ self.__precacheManifest = [
     "url": "/PortfolioWebsite/static/media/seagull_logo.72411f49.png"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/PortfolioWebsite/static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PortfolioWebsite/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/PortfolioWebsite/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/PortfolioWebsite/static/media/fontawesome-webfont.fee66e71.woff"
-  },
-  {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/PortfolioWebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PortfolioWebsite/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PortfolioWebsite/precache-manifest.338edeb4b246a02a18e899ef28e9d2aa.js"
+  "/PortfolioWebsite/precache-manifest.5a2d2cec084f06a05d99d8c8ff5c0c18.js"
 );
 
 workbox.clientsClaim();
